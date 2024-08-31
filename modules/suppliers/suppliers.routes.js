d954e7656fb00c941535c226e6852befe9031a24
omerhav <ofekmerhav08@gmail.com>
2024-08-31 21:28:29 +0300

    implement supplier routes

---
+++ modules/suppliers/suppliers.routes.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const router = express.Router();
+const productController = require('./suppliers.controller');
+
+// Define routes
+router.post('/supplier', productController.createSupplier);
+router.get('/suppliers', productController.getAllSuppliers);
+router.get('/suppliers/:id', productController.getSupplierById);
+router.put('/suppliers/:id', productController.updateSupplierById);
+router.delete('/suppliers/:id', productController.deleteSupplierById);
+
+module.exports = router;
