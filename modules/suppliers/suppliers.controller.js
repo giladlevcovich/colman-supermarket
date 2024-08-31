f00706a41c13970845cd32f3d75f4aa75790e99c
omerhav <ofekmerhav08@gmail.com>
2024-08-31 19:36:28 +0300

    add basic routes methods for supplier

---
+++ modules/suppliers/suppliers.controller.js
@@ -0,0 +1,58 @@
+const Supplier = require('supplier.module.js');
+
+
+exports.createSupplier = async (req, res) => {
+    try {
+        const supplier = new Supplier(req.body);
+        const savedSupplier = await supplier.save();
+        res.status(201).json(savedSupplier);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+exports.getAllSuppliers = async (req, res) => {
+    try {
+        const suppliers = await Supplier.find();
+        res.status(200).json(suppliers);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.getSupplierById = async (req, res) => {
+    try {
+        const supplier = await Supplier.findById(req.params.id);
+        if (!supplier) {
+            return res.status(404).json({ message: 'Supplier not found' });
+        }
+        res.status(200).json(supplier);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.updateSupplierById = async (req, res) => {
+    try {
+        //TODO: Complete
+        res.status(200).json(supplier);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+exports.deleteSupplierById = async (req, res) => {
+    try {
+        const supplier = await Supplier.findByIdAndDelete(req.params.id);
+        if (!supplier) {
+            return res.status(404).json({ message: 'Supplier not found' });
+        }
+        res.status(200).json({ message: 'Supplier deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
