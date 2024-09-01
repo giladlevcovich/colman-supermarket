320f21577dfc8b8c130f83abcff16463ab7bed38
מורן קריסטל <morank44@gmail.com>
2024-09-01 11:57:18 +0300

    add get req with filtters

--- modules/products/controller/products.controller.js
+++ modules/products/controller/products.controller.js
@@ -12,13 +12,24 @@ exports.createProduct = async (req, res) => {
 };
 
 
-exports.getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
+exports.getProducts = async (req, res) => {
+        try {
+            const { provider, name, minPrice, maxPrice, isKosher, containsGluten } = req.query;
+
+            const query = {
+                ...(provider && { provider }),
+                ...(name && { name: new RegExp(name, 'i') }),
+                ...(minPrice && { price: { $gte: minPrice } }),
+                ...(maxPrice && { price: { $lte: maxPrice } }),
+                ...(isKosher !== undefined && { isKosher: isKosher === 'true' }),
+                ...(containsGluten !== undefined && { containsGluten: containsGluten === 'true' }),
+            };
+
+            const products = await Product.find(query);
+            res.status(200).json(products);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
 };
 
 
--- modules/products/controller/products.routes.js
+++ modules/products/controller/products.routes.js
@@ -5,9 +5,10 @@ const productController = require('./products.controller');
 
 // Define routes
 router.post('/products', productController.createProduct);
-router.get('/products', productController.getAllProducts);
+router.get('/products', productController.getProducts);
 router.get('/products/:id', productController.getProductById);
 router.put('/products/:id', productController.updateProductById);
 router.delete('/products/:id', productController.deleteProductById);
+
 
 module.exports = router;
