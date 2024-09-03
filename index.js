bcb5487fb255a686bd27619bb8c9f091d3a42134
Gilad Levcovich <giladl@nimbleway.com>
2024-09-03 23:58:24 +0300

    index updated

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use(cors())
+
+const productRouter = require('./modules/products');
+const supplierRouter = require('./modules/suppliers/suppliers.routes')
+app.use('/api', productRouter);
+app.use('/api', supplierRouter)
 
 
-app.listen(80, () => {
-    console.log(`App listening on port 80`);
-})
+mongoose.connect('mongodb+srv://morankristal:Ramzi123@colman-supermarket-mong.fkyma.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(80, () => {
+        console.log(`App listening on port 80`);
+    })
+});
+
