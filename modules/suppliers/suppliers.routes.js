4cb16979312817b05a6c33169a5998f4d7acbde5
omerhav <ofekmerhav08@gmail.com>
2024-09-27 09:03:36 +0300

    add search functionally to the suppliers page

--- modules/suppliers/suppliers.routes.js
+++ modules/suppliers/suppliers.routes.js
@@ -1,14 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
-const productController = require('./suppliers.controller');
+const suppliersController = require('./suppliers.controller');
 
 // Define routes
-router.get('/suppliers', productController.getAllSuppliers);
-router.post('/supplier', productController.createSupplier);
-router.get('/supplier/:id', productController.getSupplierById);
-router.delete('/supplier/:id', productController.deleteSupplierById);
-router.put('/updateSupplier/:supplierId', productController.updateSupplierId);
-// TODO: implement the add supplier route
+router.get('/suppliers', suppliersController.getAllSuppliers);
+router.get('/suppliers/search', suppliersController.searchSuppliers);
+router.post('/supplier', suppliersController.createSupplier);
+router.get('/supplier/:id', suppliersController.getSupplierById);
+router.delete('/supplier/:id', suppliersController.deleteSupplierById);
 
 module.exports = router;
