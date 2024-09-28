cd5c9358c2a325f9a2df90cc301d78b9e886bbe5
omerhav <ofekmerhav08@gmail.com>
2024-09-28 18:45:27 +0300

    fix suppliers routes

--- modules/suppliers/suppliers.routes.js
+++ modules/suppliers/suppliers.routes.js
@@ -5,9 +5,10 @@ const suppliersController = require('./suppliers.controller');
 
 // Define routes
 router.get('/suppliers', suppliersController.getAllSuppliers);
-router.get('/suppliers/search', suppliersController.searchSuppliers);
 router.post('/supplier', suppliersController.createSupplier);
 router.get('/supplier/:id', suppliersController.getSupplierById);
 router.delete('/supplier/:id', suppliersController.deleteSupplierById);
+router.put('/updateSupplier/:supplierId', suppliersController.updateSupplierId);
+
 
 module.exports = router;
