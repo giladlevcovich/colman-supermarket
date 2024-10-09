99646059a2cf1b163df82c0993a5afe8bd74767d
Gilad Levcovich <giladl@nimbleway.com>
2024-10-09 22:32:51 +0300

    added comment to index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+// Put attention to the env file on your project. it should be .env (starts with dot)
 require('dotenv').config();
 
 app.use(express.json());
